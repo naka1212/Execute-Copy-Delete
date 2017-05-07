c1c9f0dfc31d318330d2b827f04c4221b3e03f64
naka1212 <sl@si-layout.com>
2017-05-07 15:45:05 +0900

    修正した

--- clsFtpApp/modFtpApp.vb
+++ clsFtpApp/modFtpApp.vb
@@ -2,6 +2,7 @@
 '   OBJECT NAME : メール送受信モジュール
 '   VERSION NO  : 1.0.0
 '   CREATE DATE : 2008/07/15
+'   UPDATE DATE : 2017/05/07
 '   UPDATE DATE : 
 '   SOURCE CODE COPYRIGHT : Sanae Nakagawa
 '================================================================================
