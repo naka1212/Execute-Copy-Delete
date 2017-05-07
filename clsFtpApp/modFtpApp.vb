2eb97d30ac1a81f6586a1a4361223a4811dcbb6e
naka1212 <sl@si-layout.com>
2017-05-07 15:27:14 +0900

    新規作成のためコピー

---
+++ clsFtpApp/modFtpApp.vb
@@ -0,0 +1,13 @@
+﻿'================================================================================
+'   OBJECT NAME : メール送受信モジュール
+'   VERSION NO  : 1.0.0
+'   CREATE DATE : 2008/07/15
+'   UPDATE DATE : 
+'   SOURCE CODE COPYRIGHT : Sanae Nakagawa
+'================================================================================
+Option Explicit On
+
+Module modFtpApp
+
+
+End Module
